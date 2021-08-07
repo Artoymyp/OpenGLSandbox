9e2e46ec23737bd70e8e889f22ec1a0613dc4a4e
Piatkov.Artem <artem.piatkov@intellicad.org>
2021-08-07 14:19:19 +0300

    Added code to render 'ramp' objects instead of some cubes.

--- src/camera_class.cpp
+++ src/camera_class.cpp
@@ -123,35 +123,88 @@ int main()
         Vertex(-0.5f,  0.5f,  0.5f,  0.0f, 0.0f),
         Vertex(-0.5f,  0.5f, -0.5f,  0.0f, 1.0f)
     };
+
+    std::vector<Vertex> ramp_vertices = {
+        Vertex(-0.5f, -0.5f, -0.5f,  0.0f, 0.0f),
+        Vertex( 0.5f, -0.5f, -0.5f,  1.0f, 0.0f),
+        Vertex( 0.5f,  0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex( 0.5f,  0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex(-0.5f,  0.5f, -0.5f,  0.0f, 1.0f),
+        Vertex(-0.5f, -0.5f, -0.5f,  0.0f, 0.0f),
+        
+        Vertex(-0.5f, -0.5f,  0.5f,  0.0f, 0.0f),
+        Vertex( 0.5f, -0.5f, -0.5f,  1.0f, 0.0f),
+        Vertex( 0.5f,  0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex( 0.5f,  0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex(-0.5f,  0.5f,  0.5f,  0.0f, 1.0f),
+        Vertex(-0.5f, -0.5f,  0.5f,  0.0f, 0.0f),
+        
+        Vertex(-0.5f,  0.5f,  0.5f,  1.0f, 0.0f),
+        Vertex(-0.5f,  0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex(-0.5f, -0.5f, -0.5f,  0.0f, 1.0f),
+        Vertex(-0.5f, -0.5f, -0.5f,  0.0f, 1.0f),
+        Vertex(-0.5f, -0.5f,  0.5f,  0.0f, 0.0f),
+        Vertex(-0.5f,  0.5f,  0.5f,  1.0f, 0.0f),
+        
+        Vertex(-0.5f, -0.5f,  0.5f,  1.0f, 0.0f),
+        Vertex( 0.5f, -0.5f, -0.5f,  1.0f, 1.0f),
+        Vertex(-0.5f, -0.5f, -0.5f,  0.0f, 1.0f),
+        Vertex(-0.5f,  0.5f,  0.5f,  0.0f, 1.0f),
+        Vertex( 0.5f,  0.5f, -0.5f,  0.0f, 0.0f),
+        Vertex(-0.5f,  0.5f, -0.5f,  1.0f, 0.0f),
+    };
     // world space positions of our cubes
     std::vector<glm::vec3> cubePositions = {
         glm::vec3( 0.0f,  0.0f,  0.0f),
         glm::vec3( 2.0f,  5.0f, -15.0f),
         glm::vec3(-1.5f, -2.2f, -2.5f),
         glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3( 2.4f, -0.4f, -3.5f)
+    };
+
+    std::vector<glm::vec3> ramp_positions = {
         glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
+
     unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
 
-    glBindVertexArray(VAO);
+        glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // texture coord attribute
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
 
+    unsigned int VBO1, VAO1;
+    {
+        glGenVertexArrays(1, &VAO1);
+        glGenBuffers(1, &VBO1);
+
+        glBindVertexArray(VAO1);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+        glBufferData(GL_ARRAY_BUFFER, ramp_vertices.size() * sizeof(Vertex), &ramp_vertices[0], GL_STATIC_DRAW);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // texture coord attribute
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
 
     // load and create a texture 
     // -------------------------
@@ -254,6 +307,19 @@ int main()
             // calculate the model matrix for each object and pass it to shader before drawing
             glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
             model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+        }
+        // render ramps
+        glBindVertexArray(VAO1);
+        for (unsigned int i = 0; i < ramp_positions.size(); i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+            model = glm::translate(model, ramp_positions[i]);
             float angle = 20.0f * i;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
